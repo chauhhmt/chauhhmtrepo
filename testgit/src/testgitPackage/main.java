4573a61e044230d3b7266131b56a195a0a741e5f
chau nguyen <chau@hhmt.com.au>
2021-07-06 11:15:21 +1000

    Update main.java

--- testgit/src/testgitPackage/main.java
+++ testgit/src/testgitPackage/main.java
@@ -1,6 +1,11 @@
 package testgitPackage;
 
 public class main {
-	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("today check 06-07-2021");
+
+	}
 
 }
